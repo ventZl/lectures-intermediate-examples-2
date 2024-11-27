2b6c128a67bc4254ac271a447e04b3750b79c9e1
n <e>
2024-11-27 15:01:43 +0100

    Add example of cooperative task switcher

--- after/09_05_coroutine/coroutine.cpp
+++ after/09_05_coroutine/coroutine.cpp
@@ -1,25 +1,47 @@
 #include <coroutine>
 #include <iostream>
+#include <vector>
 
 struct promise;
 struct range : std::coroutine_handle<promise> { using promise_type = ::promise; };
 
+std::vector<range> tasks;
+int current_task = 0;
+
 struct promise {
+    using handle_type = std::coroutine_handle<promise>;
+    struct task_switcher {
+        task_switcher(handle_type & next) : next_coro{ next } {}
+        bool await_ready() { return false; }
+        auto await_suspend(std::coroutine_handle<>) { return next_coro; }
+        void await_resume() {}
+
+        handle_type & next_coro;
+    };
     range get_return_object() { return {range::from_promise(*this)}; }
     std::suspend_always initial_suspend() noexcept { return {}; }
-    std::suspend_always final_suspend() noexcept { return {}; }
-    std::suspend_always yield_value(int i) { value = i; return {}; }
-    void return_value(int i) { value = i; }
+    std::suspend_always final_suspend() noexcept {
+        return {};
+    }
+    task_switcher yield_value(int) {
+        current_task = (current_task + 1) % tasks.size();
+        return { tasks[current_task] };
+    }
+    void return_void() { }
     void unhandled_exception() {}
 
     int value;
 };
 
 int main(int, char **) {
-range r = [](int from, int to) -> range {
-    while (from < to) { co_yield from++; } co_return from;
-}(0, 100);
+    tasks.emplace_back([]() -> range {
+        while (true) { std::cout << "First!" << std::endl; co_yield{}; } co_return;
+    }());
 
-while (!r.done()) { r.resume(); std::cout << r.promise().value << std::endl; }
+    tasks.emplace_back([]() -> range {
+        while (true) { std::cout << "Second!" << std::endl; co_yield{}; } co_return;
+        }());
+
+    tasks[0].resume();
     return 0;
 }
